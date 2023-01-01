e12130b67eccb77b4c4f17acf62d2446d7905050
yassertawfik4 <106037940+yassertawfik4@users.noreply.github.com>
2023-01-01 23:03:01 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,26 @@
+ScrollReveal({
+    rest:true,
+    distance:'80px',
+    duration: 2000,
+    delay: 400
+});
+ScrollReveal().reveal('.nav', { delay: 500, origin: 'top' });
+ScrollReveal().reveal('.tit', { delay: 500, origin: 'left' });
+ScrollReveal().reveal('.right', { delay: 500, origin: 'right' });
+
+
+///////////////////
+var typed =new Typed(".typing",{
+    strings:["DR.SHAHIRA EL GHALBAN","د/شهيره الغلبان"],
+    typeSpeed:100,
+    backSpeed:70,
+    loop:true
+})
+//////////////
+
+// var land=document.querySelector('.landing')
+// var home=document.querySelector('.Home')
+
+// home.addEventListener('click',()=>{
+//     land.classList.remove('active')
+// })
